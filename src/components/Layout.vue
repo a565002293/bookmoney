4f1399422453e1f3ff27c305fd2b8604f50f64f0
LDQ <2693129576@qq.com>
2020-02-14 21:25:47 +0800

    添加nav样式

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -15,13 +15,11 @@ export default {
 
 <style lang="scss" scoped>
 .nav-wrapper {
-  border: 1px solid red;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
 }
 .content {
-  border: 1px solid green;
   overflow: auto;
   flex-grow: 1;
 }
