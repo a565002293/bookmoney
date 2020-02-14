86d3542aea1de47a6cfde9d6fd57a96826ff6bf6
LDQ <2693129576@qq.com>
2020-02-14 22:22:29 +0800

    解决svg自带属性的问题

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,10 @@ module.exports = {
       .use("svg-sprite-loader")
       .loader("svg-sprite-loader")
       .options({ extract: false })
-      .end();
+      .end()
+      .use('svgo-loader').loader('svgo-loader')
+        .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end();
+
     config
       .plugin("svg-sprite")
       .use(require("svg-sprite-loader/plugin"), [{ plainSprite: true }]);
