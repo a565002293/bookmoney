2f030d4c823af264d857304b9b50af297dc701c4
LDQ <2693129576@qq.com>
2020-02-15 19:58:53 +0800

    使用ts实现了支出收入切换组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,41 +1,52 @@
 <template>
-    <div>
-        <ul class="types">
-            <li class="selected">支出</li>
-            <li class="selected">收入</li>
-        </ul>
-    </div>
-
+  <div>
+    <ul class="types">
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: "Types"
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component
+export default class Types extends Vue {
+  type = "-";
+  selectType(type: string) {
+    if (type !== "-" && type !== "+") {
+      throw new Error("type is unknown");
     }
+    this.type = type;
+  }
+}
+// export default {
+//     name: "Types"
+// }
 </script>
 
 <style lang="scss" scoped>
-    .types {
-        background: #c4c4c4;
-        display: flex;
-        text-align: center;
-        font-size: 24px;
-        > li {
-            width: 50%;
-            height: 64px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: relative;
-            &.selected::after {
-                content: '';
-                position: absolute;
-                bottom: 0;
-                left: 0;
-                width: 100%;
-                height: 4px;
-                background: #333;
-            }
-        }
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
     }
+  }
+}
 </style>
