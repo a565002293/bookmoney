07da30010c30000406094b857966b57051136c9a
LDQ <2693129576@qq.com>
2020-02-15 14:51:49 +0800

    添加reset.css

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -26,6 +26,7 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
   nav {
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -44,7 +45,7 @@
       }
     }
     > .item.selected{
-      color: grey;
+      color: $color-highlight;
     }
   }
 </style>
