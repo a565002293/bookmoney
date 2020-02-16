97b32685a3f01cb1085e32599bf6f224df710396
LDQ <2693129576@qq.com>
2020-02-16 12:25:31 +0800

    解决了用户点击取消也会输入的bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -28,11 +28,16 @@
         }
         create(){
             const name = window.prompt('请输入标签名');
-            if(name === ''){
-                window.alert('标签名不能为空')
-            }else if(this.dataSource){
-                this.$emit('update:dataSource',[...this.dataSource, name])
+            if (name !== null){
+                if(name === ''){
+                    window.alert('标签名不能为空')
+                }else if(this.dataSource){
+                    this.$emit('update:dataSource',[...this.dataSource, name])
+                }
+            }else{
+                return false
             }
+
         }
     }
 </script>
