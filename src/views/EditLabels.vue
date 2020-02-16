0451f67081340d6fe7f6cfef0eaef73a8e5644dd
LDQ <2693129576@qq.com>
2020-02-16 22:56:26 +0800

    还没有同步money的tags和label的内容

--- src/views/EditLabels.vue
+++ src/views/EditLabels.vue
@@ -1,35 +1,9 @@
 <template>
     <Layout>
+        <Icon name="left"/>
         hi
     </Layout>
 </template>
-
-<!--<script lang="ts">-->
-<!--    import Vue from "vue";-->
-<!--    import {Component} from "vue-property-decorator";-->
-<!--    import {tagListModel} from "@/model/tagListModel";-->
-
-<!--    @Component-->
-<!--    export default class EditLabels extends Vue {-->
-<!--        created(){-->
-<!--           const id =this.$route.params.id;-->
-<!--            tagListModel.fetch();-->
-<!--           const tags = tagListModel.data;-->
-<!--           const tag = tags.filter(t => t.id === id)[0];-->
-<!--           if(tag){-->
-<!--               console.log(tag)-->
-<!--           } else {-->
-<!--               this.$router.replace('/404')-->
-<!--           }-->
-<!--        }-->
-<!--    }-->
-<!--</script>-->
-
-<!--<template>-->
-<!--    <Layout>-->
-<!--        编辑标签-->
-<!--    </Layout>-->
-<!--</template>-->
 
 <script lang="ts">
     import Vue from 'vue';
