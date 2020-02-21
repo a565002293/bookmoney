e467e819dd6570b4033b27de814c2e9bc4c6b9f9
LDQ <2693129576@qq.com>
2020-02-22 00:09:07 +0800

    修复numberpad的bug

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -18,52 +18,45 @@
             <button @click="inputContent">.</button>
         </div>
     </div>
-
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
-    import { Component } from "vue-property-decorator";
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
     @Component
     export default class NumberPad extends Vue {
-        output = '0';
-
+        @Prop(Number) readonly value!: number;
+        output = this.value.toString();
         inputContent(event: MouseEvent) {
             const button = (event.target as HTMLButtonElement);
             const input = button.textContent!;
-            if (this.output.length === 16) {
-                return
-            }
+            if (this.output.length === 16) { return; }
             if (this.output === '0') {
                 if ('0123456789'.indexOf(input) >= 0) {
                     this.output = input;
                 } else {
-                    this.output += input
+                    this.output += input;
                 }
                 return;
             }
-            if (this.output.indexOf('.') >= 0 && input === '.') {
-                return;
-            }
-            this.output += input
+            if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+            this.output += input;
         }
-
         remove() {
             if (this.output.length === 1) {
-                this.output = '0'
+                this.output = '0';
             } else {
                 this.output = this.output.slice(0, -1);
             }
         }
-
         clear() {
-            this.output = '0'
+            this.output = '0';
         }
-
         ok() {
-            this.output = '0'
-            this.$emit('update:value', this.output);
-            this.$emit('submit',this.output);
+            const number = parseFloat(this.output);
+            this.$emit('update:value', number);
+            this.$emit('submit', number);
+            this.output = '0';
         }
     }
 </script>
@@ -95,7 +88,7 @@
                 &.zero {
                     width: 25*2%;
                 }
-                $bg: #f2f2f2;
+                $bg: #F2F2F2;
                 &:nth-child(1) {
                     background: $bg;
                 }
@@ -120,5 +113,4 @@
             }
         }
     }
-
 </style>
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,10 +1,18 @@
+
+type RootState = {
+    recordList: RecordItem[];
+    tagList: Tag[];
+    currentTag?: Tag;
+}
+
 type RecordItem = {
     tags: string[];
     notes: string;
     type: string;
     amount: number;
-    createdAt?: Date;
+    createdAt?: string;
 }
+
 type Tag ={
     id: string;
     name: string;
