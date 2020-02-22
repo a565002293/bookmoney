656fc4e358935d6308053eabdf6541a158f8d489
LDQ <2693129576@qq.com>
2020-02-22 19:41:24 +0800

    修改了两个bug，基本完成

--- src/store/index.ts
+++ src/store/index.ts
@@ -67,11 +67,13 @@ const store = new Vuex.Store({
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
         window.alert('标签名重复了');
+      }else {
+        const id = createId().toString();
+        state.tagList.push({id, name: name});
+        store.commit('saveTags');
+        window.alert('添加成功');
       }
-      const id = createId().toString();
-      state.tagList.push({id, name: name});
-      store.commit('saveTags');
-      window.alert('添加成功');
+
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
--- src/store/tagStore.ts
+++ src/store/tagStore.ts
@@ -11,18 +11,6 @@ const tagStore = {
     findTag(id: string) {
         return this.tagList.filter(t => t.id === id)[0];
     },
-    createTag(name: string) {
-        const names = this.tagList.map(item => item.name);
-        if (names.indexOf(name) >= 0) {
-            window.alert('标签名重复了');
-            return 'duplicated';
-        }
-        const id = createId().toString();
-        this.tagList.push({id, name: name});
-        this.saveTags();
-        window.alert('添加成功');
-        return 'success';
-    },
     removeTag(id: string) {
         let index = -1;
         for (let i = 0; i < this.tagList.length; i++) {
